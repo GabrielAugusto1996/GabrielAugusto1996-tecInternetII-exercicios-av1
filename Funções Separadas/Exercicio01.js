b5278288a23a965464f6e09f4a9ef659fb4f6cf2
Gabriel Augusto <biga00145@gmail.com>
2017-08-21 21:35:52 -0300

    Exercicio resolvido

--- Funções Separadas/Exercicio01.js
+++
@@ -1,9 +0,0 @@
-function obterMedia(primeiroNumero, segundoNumero) {
-	
-	var media = parseFloat((parseInt(primeiroNumero) + parseInt(segundoNumero)) / 2);
-	
-	console.log("A média é:" + media);
-	
-}
-
-obterMedia(2, 5);
